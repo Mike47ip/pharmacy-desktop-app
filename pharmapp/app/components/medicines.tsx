bbe19945e79d2d268368d3b9ed8d29da74155766
Mike Dark <agyemankojo47@gmail.com>
2024-06-14 17:12:26 +0000

    Make medicine cards responsive

--- pharmapp/app/components/medicines.tsx
+++ pharmapp/app/components/medicines.tsx
@@ -14,132 +14,133 @@ const Medicines = () => {
   <>
    <article className="pt-6 px-4">
     <h2 className="font-bold text-lg pl-4">Medicines</h2>
-    <ul className="grid grid-cols-2 pt-5">
-     <li className="mx-2">
-      <Image
-       src="/para.jpg"
-       alt="para"
-       className="rounded-lg"
-       width={170}
-       height={150}
-      />
-      <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
-      <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
-       Paracetamol works by inhibiting the production of certain chameical..
-      </div>
-      <span className="text-[0.6rem] font-bold">Stock:12</span>
-      <div className="flex justify-between items-center w-44">
-       <div className="flex">
-        <span className="text-custom-green pr-1 align-super text-xs font-semibold">
-         $
-        </span>
-        <p className="font-bold font-open-sans">3.75</p>
+    <div className="flex justify-center items-center">
+     <ul className="grid grid-cols-2 pt-2">
+      <li className="mx-2 pt-3">
+       <Image
+        src="/para.jpg"
+        alt="para"
+        className="rounded-lg object-cover"
+        layout="fill"
+       />
+       <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
+       <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
+        Paracetamol works by inhibiting the production of certain chameical..
        </div>
-       <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
-        <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
-         <span className="text-custom-green font-semibold  text-lg">-</span>
+       <span className="text-[0.6rem] font-bold">Stock:12</span>
+       <div className="flex justify-between items-center w-44">
+        <div className="flex">
+         <span className="text-custom-green pr-1 align-super text-xs font-semibold">
+          $
+         </span>
+         <p className="font-bold font-open-sans">3.75</p>
         </div>
-        <span className="text-[0.5rem] font-semibold">1</span>
-        <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
-         <span className="pb-[1.1px] font-semibold text-lg">+</span>
-        </div>
-       </div>
-      </div>
-     </li>
-     <li className="mx-2">
-      <Image
-       src="/para.jpg"
-       alt="para"
-       className="rounded-lg"
-       width={170}
-       height={150}
-      />
-      <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
-      <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
-       Paracetamol works by inhibiting the production of certain chameical..
-      </div>
-      <span className="text-[0.6rem] font-bold">Stock:12</span>
-      <div className="flex justify-between items-center w-44">
-       <div className="flex">
-        <span className="text-custom-green pr-1 align-super text-xs font-semibold">
-         $
-        </span>
-        <p className="font-bold font-open-sans">3.75</p>
-       </div>
-       <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
-        <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
-         <span className="text-custom-green font-semibold  text-lg">-</span>
-        </div>
-        <span className="text-[0.5rem] font-semibold">1</span>
-        <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
-         <span className="pb-[1.1px] font-semibold text-lg">+</span>
+        <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
+         <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+          <span className="text-custom-green font-semibold  text-lg">-</span>
+         </div>
+         <span className="text-[0.5rem] font-semibold">1</span>
+         <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
+          <span className="pb-[1.1px] font-semibold text-lg">+</span>
+         </div>
         </div>
        </div>
-      </div>
-     </li>
-     <li className="mx-2">
-      <Image
-       src="/para.jpg"
-       alt="para"
-       className="rounded-lg"
-       width={170}
-       height={150}
-      />
-      <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
-      <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
-       Paracetamol works by inhibiting the production of certain chameical..
-      </div>
-      <span className="text-[0.6rem] font-bold">Stock:12</span>
-      <div className="flex justify-between items-center w-44">
-       <div className="flex">
-        <span className="text-custom-green pr-1 align-super text-xs font-semibold">
-         $
-        </span>
-        <p className="font-bold font-open-sans">3.75</p>
+      </li>
+      <li className="mx-2 pt-3">
+       <Image
+        src="/para.jpg"
+        alt="para"
+        className="rounded-lg"
+        width={170}
+        height={150}
+       />
+       <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
+       <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
+        Paracetamol works by inhibiting the production of certain chameical..
        </div>
-       <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
-        <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
-         <span className="text-custom-green font-semibold  text-lg">-</span>
+       <span className="text-[0.6rem] font-bold">Stock:12</span>
+       <div className="flex justify-between items-center w-44">
+        <div className="flex">
+         <span className="text-custom-green pr-1 align-super text-xs font-semibold">
+          $
+         </span>
+         <p className="font-bold font-open-sans">3.75</p>
         </div>
-        <span className="text-[0.5rem] font-semibold">1</span>
-        <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
-         <span className="pb-[1.1px] font-semibold text-lg">+</span>
-        </div>
-       </div>
-      </div>
-     </li>
-     <li className="mx-2">
-      <Image
-       src="/para.jpg"
-       alt="para"
-       className="rounded-lg"
-       width={170}
-       height={150}
-      />
-      <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
-      <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
-       Paracetamol works by inhibiting the production of certain chameical..
-      </div>
-      <span className="text-[0.6rem] font-bold">Stock:12</span>
-      <div className="flex justify-between items-center w-44">
-       <div className="flex">
-        <span className="text-custom-green pr-1 align-super text-xs font-semibold">
-         $
-        </span>
-        <p className="font-bold font-open-sans">3.75</p>
-       </div>
-       <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
-        <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
-         <span className="text-custom-green font-semibold  text-lg">-</span>
-        </div>
-        <span className="text-[0.5rem] font-semibold">1</span>
-        <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
-         <span className="pb-[1.1px] font-semibold text-lg">+</span>
+        <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
+         <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+          <span className="text-custom-green font-semibold  text-lg">-</span>
+         </div>
+         <span className="text-[0.5rem] font-semibold">1</span>
+         <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
+          <span className="pb-[1.1px] font-semibold text-lg">+</span>
+         </div>
         </div>
        </div>
-      </div>
-     </li>
-    </ul>
+      </li>
+      <li className="mx-2 pt-3">
+       <Image
+        src="/para.jpg"
+        alt="para"
+        className="rounded-lg"
+        width={170}
+        height={150}
+       />
+       <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
+       <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
+        Paracetamol works by inhibiting the production of certain chameical..
+       </div>
+       <span className="text-[0.6rem] font-bold">Stock:12</span>
+       <div className="flex justify-between items-center w-44">
+        <div className="flex">
+         <span className="text-custom-green pr-1 align-super text-xs font-semibold">
+          $
+         </span>
+         <p className="font-bold font-open-sans">3.75</p>
+        </div>
+        <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
+         <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+          <span className="text-custom-green font-semibold  text-lg">-</span>
+         </div>
+         <span className="text-[0.5rem] font-semibold">1</span>
+         <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
+          <span className="pb-[1.1px] font-semibold text-lg">+</span>
+         </div>
+        </div>
+       </div>
+      </li>
+      <li className="mx-2 pt-3">
+       <Image
+        src="/para.jpg"
+        alt="para"
+        className="rounded-lg"
+        width={170}
+        height={150}
+       />
+       <p className="pt-3 text-sm font-semibold">Paracetamol Berno</p>
+       <div className=" w-36 text-[0.7rem] leading-snug text-custom-gray">
+        Paracetamol works by inhibiting the production of certain chameical..
+       </div>
+       <span className="text-[0.6rem] font-bold">Stock:12</span>
+       <div className="flex justify-between items-center w-44">
+        <div className="flex">
+         <span className="text-custom-green pr-1 align-super text-xs font-semibold">
+          $
+         </span>
+         <p className="font-bold font-open-sans">3.75</p>
+        </div>
+        <div className="bg-slate-100 w-20 p-1 flex justify-between items-center rounded-2xl">
+         <div className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+          <span className="text-custom-green font-semibold  text-lg">-</span>
+         </div>
+         <span className="text-[0.5rem] font-semibold">1</span>
+         <div className="bg-custom-green w-6 h-6 flex justify-center items-center text-white rounded-full">
+          <span className="pb-[1.1px] font-semibold text-lg">+</span>
+         </div>
+        </div>
+       </div>
+      </li>
+     </ul>
+    </div>
    </article>
   </>
  );
