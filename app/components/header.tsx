44b20ceb3c1d5c8b22b720107d798a3d876f455c
Mike Dark <agyemankojo47@gmail.com>
2024-07-18 21:02:20 +0000

    Make app fully responsive

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -56,7 +56,7 @@ export default function Header() {
 
     <div className="flex justify-center items-center font-normal border-[0.5px] gap-1 p-2 border-slate-200 rounded-lg">
      <Image src="/voucher.png" alt="voucher" width={23} height={23} />
-     <p className="whitespace-nowrap text-sm font-semibold hidden lg:block">
+     <p className="whitespace-nowrap text-sm font-semibold hidden lg1:hidden lg2:block lg:block">
       Use Voucher
      </p>
     </div>
@@ -70,8 +70,8 @@ export default function Header() {
      >
       <path d="M40-120v-200h80v120h120v80H40Zm680 0v-80h120v-120h80v200H720ZM160-240v-480h80v480h-80Zm120 0v-480h40v480h-40Zm120 0v-480h80v480h-80Zm120 0v-480h120v480H520Zm160 0v-480h40v480h-40Zm80 0v-480h40v480h-40ZM40-640v-200h200v80H120v120H40Zm800 0v-120H720v-80h200v200h-80Z" />
      </svg>
-     <p className="whitespace-nowrap text-sm font-semibold hidden lg:block">
-      Scan Prescription
+     <p className="whitespace-nowrap text-sm font-semibold hidden lg1:hidden lg2:block lg:block">
+      Scan <span className="">Prescription</span> 
      </p>
     </div>
     <div className="flex gap-0 border-[0.5px] p-2 border-slate-200 rounded-lg">
