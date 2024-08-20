d6f42d5b02a6b095e266198071bd5a4686e8654b
Mike Dark <agyemankojo47@gmail.com>
2024-08-20 19:13:56 +0000

    Style login page

--- app/components/login/login.tsx
+++ app/components/login/login.tsx
@@ -1,45 +1,61 @@
 "use client";
 
+import Image from "next/image";
+
 import { useRouter } from "next/navigation";
 
 const Login = () => {
-  const router = useRouter(); // Initialize the router
+ const router = useRouter(); // Initialize the router
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Add login logic here (e.g., authentication)
-    router.push("/home"); // Redirect to /home after successful login
-  };
+ const handleSubmit = (event: React.FormEvent) => {
+  event.preventDefault();
+  // Add login logic here (e.g., authentication)
+  router.push("/home"); // Redirect to /home after successful login
+ };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md">
-        <h1 className="text-2xl font-bold mb-6">Login</h1>
-        <form onSubmit={handleSubmit}> {/* Attach handleSubmit to form */}
-          <div className="mb-4">
-            <label className="block text-gray-700">Username</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700">Password</label>
-            <input
-              type="password"
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-custom-green text-white p-2 rounded"
-          >
-            Login
-          </button>
-        </form>
-      </div>
+ return (
+  <>
+   <div className="flex w-full h-screen bg-custom-background  ">
+    <div className="w-[50%] h-full">
+     <Image
+      src="/boxes2.jpg"
+      alt="shopping cart"
+      className="object-cover w-full h-full"
+      width={250}
+      height={220}
+     />
     </div>
-  );
+    <div className="flex flex-col justify-center items-center px-20 rounded shadow-md w-[50%]">
+     <h1 className="text-2xl font-bold mb-6">Sign in to Salesai</h1>
+     <form onSubmit={handleSubmit} className="w-full px-20">
+      <div className="mb-4 bg-custom-logininput">
+       <input
+        type="text"
+        className="w-full py-2 px-4 border border-gray-300 rounded-3xl"
+        placeholder="Email"
+       />
+      </div>
+      <div className="mb-6">
+       <input
+        type="password"
+        className="w-full py-2 px-4 border border-gray-300 rounded-3xl"
+        placeholder="Password"
+       />
+      </div>
+      <button
+       type="submit"
+       className="w-full bg-custom-green text-white p-2 rounded-3xl"
+      >
+       Sign in
+      </button>
+     </form>
+    <div className="pt-1">
+    <span>Do not have an account?</span> <button className="text-custom-green font-semibold">Sign Up now</button>
+    </div>
+    </div>
+   </div>
+  </>
+ );
 };
 
 export default Login;
